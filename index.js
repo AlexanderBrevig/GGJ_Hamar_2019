3c55468f22f307ae698fbf6735b2cb98f0c980ea
Alexander Brevig <alexander@bitwave.no>
2019-01-26 04:55:00 +0100

    add task parse and refactor

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require("socket.io")(http);
 var art = require("ascii-art");
 
 var namespaces = [];
+var users = [];
 
 let d = msg => {
   console.log(msg);
@@ -19,9 +20,8 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
   d("a user connected public");
 
-  setupCreateRoom(socket);
-
-  setupJoinRoom(socket);
+  setupCreateRoom(socket); //used by admin
+  setupJoinRoom(socket); //used by players
 
   socket.on("disconnect", () => {
     d("user disconnected public");
@@ -32,6 +32,9 @@ http.listen(3000, () => {
   d("listening on *:3000");
 });
 
+/*
+    SETUP SOCKET LISTENERS AND CALLBACKS
+*/
 function setupCreateRoom(socket) {
   socket.on("createRoom", msg => {
     let roomId = Math.random()
@@ -39,7 +42,8 @@ function setupCreateRoom(socket) {
       .substring(7);
     if (namespaces[roomId] === undefined) {
       namespaces[roomId] = io.of("/" + roomId);
-      setupRoom(namespaces[roomId]);
+      users[roomId] = ["test1", "test2", "test3"];
+      setupServerRoom(namespaces, roomId); //TODO refactor
     }
     d("room: " + roomId);
     art.font("join      " + roomId, "Doom", rendered => {
@@ -63,24 +67,67 @@ function setupJoinRoom(socket) {
   });
 }
 
-function setupRoom(room) {
+function setupServerRoom(spaces, roomId) {
+  var room = spaces[roomId];
   room.on("connection", socket => {
-    socket.on("joined", user => {
-      d(user + " joined requested room");
-      if (users[user] === undefined) {
-        room.emit("joined", user);
-        room.emit("task", {
-          targets: ["everyone"],
-          text: "Click the button 10 times",
-          action: "clicker",
-          timeLimit: 10
-        });
-      } else {
-        socket.emit("joinError", "User exists");
-      }
+    setupRoomJoin(socket, roomId);
+    socket.on("task", task => {
+      var taskObject = parseTask(task);
+      room.emit("task", taskObject);
     });
     socket.on("disconnect", () => {
       d("user disconnected room");
     });
   });
+
+  function setupRoomJoin(socket, roomId) {
+    socket.on("joined", user => {
+      d(user + " joined room " + roomId);
+      if (!users[roomId].includes(user)) {
+        users[roomId].push(user);
+        d(users[roomId]);
+        room.emit("joined", user);
+      } else {
+        socket.emit("joinError", "User exists");
+      }
+    });
+  }
+
+  function parseTask(task) {
+    d("PARSE: " + task);
+    var taskObject = {
+      players: ["everyone"],
+      action: "",
+      objective: "",
+      modifier: "",
+      text: ""
+    };
+    task = task.split(" ");
+    if (task[0] === "crack") {
+      taskObject.action = task[0];
+      taskObject.players = users[roomId]
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 3);
+      taskObject.objective = task[1];
+      taskObject.modifier = task[2];
+    } else if (task[0] === "all") {
+      taskObject.action = task[1];
+      if (task[1] === "click") {
+        taskObject.objective = task[2];
+        if (task[3] !== undefined) {
+          taskObject.modifier = task[3]
+            .replace("(", "")
+            .replace(")", "")
+            .split(",");
+        }
+      }
+      if (task[1] === "hold") {
+        taskObject.objective = task[2];
+      }
+      if (task[1] === "count") {
+        taskObject.objective = task[2];
+      }
+    }
+    return taskObject;
+  }
 }
